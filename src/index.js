b312136791a7d4e2b69b3c2f0f2675bd51097d1d
Misha Gorban <mixei8189@gmail.com>
2022-08-20 08:29:53 +0300

    add function connet from react-redax

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createStore, bindActionCreators } from 'redux'
+import { createStore } from 'redux'
 import reducer from './reducer';
-import * as actions from './actions'
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-
-
-const update = () => {
-	document.getElementById('counter').textContent = getState().value;
-}
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-// 	dispatch(creator(...args));
-// }
-
-
-
-
-const { inc, dec, rnd } = bindActionCreators( actions, dispatch );
-
-document.getElementById('inc').addEventListener('click', inc);
-
-
-document.getElementById('dec').addEventListener('click', dec)
-
-
-document.getElementById('rnd').addEventListener('click', () => {
-	const value = Math.floor(Math.random() * 10);
-	rnd(value);
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<>
-
-		</>
+		{/* <Counter
+				counter={getState().value}
+				inc={inc}
+				dec={dec}
+				rnd={() => {
+					const value = Math.floor(Math.random() * 10);
+					rnd(value);
+				}}
+			/> */}
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
+
+
+
+
+
 
